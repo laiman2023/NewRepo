291d1ec9a1547c76d8532cd34957b571431def14
laiman <anas.sadeq@tekschool.us>
2023-02-22 11:53:12 +1100

    number two git practice

---
+++ src/GItPackage/GitTwo.java
@@ -0,0 +1,13 @@
+package GItPackage;
+
+public class GitTwo {
+
+	public static void main(String[] args) {
+		
+		String str = "This is for push practice";
+		System.out.println(str);
+		
+		
+	}
+
+}

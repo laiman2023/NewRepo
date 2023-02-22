e294651ff24efe1eea61751791b8695e9ed3f638
laiman <anas.sadeq@tekschool.us>
2023-02-22 20:09:00 +1100

    this is git3 practice

--- src/GItPackage/testBranch.java
+++ src/GItPackage/testBranch.java
@@ -4,8 +4,10 @@ public class testBranch {
 	
 	public static void testwo() {
 		System.out.println("this is for branch practice");
-		String str1 = "I do not fucking know when Java will click in my mind"
-			System.out.println(str);
+		
+		String str1 = "I do not fucking know when Java will click in my mind";
+			System.out.println(str1);
+			
 		
 	}
 	

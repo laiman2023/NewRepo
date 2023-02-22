83df3cb48a90b3ef6bf17a15044c16f53f0bc1d2
laiman <anas.sadeq@tekschool.us>
2023-02-22 11:43:27 +1100

    new methode added

--- src/GItPackage/FirstGet.java
+++ src/GItPackage/FirstGet.java
@@ -5,8 +5,13 @@ public class FirstGet {
 	public static void main(String[] args) {
 		int a  = 10;
 		int b = 30;
-		System.out.println(a*b);
 		
+		
+		System.out.println(testOne(10,5));
+	}
+	
+	public static int testOne(int a, int b) {
+		return a-b;
 	}
 
 }
